75ab7bc6c5294a2c19cf0d3bd9311284355b074d
Adrian Łaciak <adrianlaciak02@gmail.com>
2026-01-01 23:30:49 +0100

    Celsius to Fahrenheit

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Fahrenheit;
+
+/*Napisz program przeliczający temperaturę w stopniach Celsjusza na temperaturę w
+stopniach Fahrenheita. Program ma prosić użytkownika o podanie temperatury w stopniach
+Celsjusza. Wzór: F = 32 + 9/5C.*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Proszę wpisać temperaturę w stopniach Celsjusza: ");
+        double celsjusz = double.Parse(Console.ReadLine());
+        double fahrenheit = 32 + celsjusz * 9.0 / 5.0;
+        Console.WriteLine($"{celsjusz} stopni Celsjusza to {fahrenheit} stopni Fahrenheit'a.");
+    }
+}
