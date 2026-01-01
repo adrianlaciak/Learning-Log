889a9d407aa0e017431f07aee2921c797954fb90
Adrian Łaciak <adrianlaciak02@gmail.com>
2026-01-02 00:04:00 +0100

    Fixed Delta

--- CSharp/Delta/Program.cs
+++ CSharp/Delta/Program.cs
@@ -9,10 +9,13 @@ class Program
     {
         Console.WriteLine("Proszę podać a:");
         double a = double.Parse(Console.ReadLine());
+        
         Console.WriteLine("Proszę podać b:");
         double b = double.Parse(Console.ReadLine());
+        
         Console.WriteLine("Proszę podać c:");
         double c = double.Parse(Console.ReadLine());
+        
         double delta = Math.Pow(b, 2) - 4 * a * c;
         Console.WriteLine($"Delta dla równania: {a}x² + {b}x + {c} = 0 wynosi {Math.Round(delta, 2)}.");
     }
