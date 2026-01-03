8f156734397c5bfd23adbce025e9997f9385cb38
Adrian Łaciak <adrianlaciak02@gmail.com>
2026-01-03 18:32:39 +0100

    Leap year

---
+++ CSharp/LeapYear/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LeapYear;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Program sprawdzający czy podany rok jest rokiem przestępnym.");
+        Console.WriteLine();
+
+        Console.WriteLine("Proszę podać rok");
+        int rok =  int.Parse(Console.ReadLine());
+
+        if (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0)
+        {
+            Console.WriteLine($"Rok {rok} jest przestępny.");
+        }
+        else
+        {
+            Console.WriteLine($"Rok {rok} nie jest przestępny.");
+        }
+    }
+}
