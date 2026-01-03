29e29f12b7a393337089c7891a31fd0e1f1da880
Adrian Łaciak <adrianlaciak02@gmail.com>
2026-01-03 18:22:42 +0100

    BMI calculator

---
+++ CSharp/BMI/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace BMI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kalkulator BMI");
+        Console.WriteLine();
+        
+        Console.Write("Proszę podać wagę w kilogramach (np. 70,50): ");
+        double masa = double.Parse(Console.ReadLine());
+        
+        Console.Write("Proszę podać wzrost w centymetrach (np. 180,2): ");
+        double wzrost_cm = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        
+        double wzrost = wzrost_cm / 100.0;
+        double bmi = Math.Round(masa / Math.Pow(wzrost, 2), 2);
+        if (bmi > 30)
+        {
+            Console.WriteLine("---Otyłość---");
+        }
+        else if (bmi >= 25)
+        {
+            Console.WriteLine("---Nadwaga---");
+        }
+        else if (bmi >= 18.5)
+        {
+            Console.WriteLine("---Prawidłowa masa ciała---");
+        }
+        else
+        {
+            Console.WriteLine("---Niedowaga---");
+        }
+        Console.WriteLine($"Twoje bmi wynosi {bmi}.");
+    }
+}
