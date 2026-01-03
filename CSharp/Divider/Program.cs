b37ee4c55c6dbff3f402876db1807d51338d6041
Adrian Łaciak <adrianlaciak02@gmail.com>
2026-01-03 18:39:18 +0100

    Divider

---
+++ CSharp/Divider/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Divider;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Program sprawdzający czy druga liczba jest dzielnikiem pierwszej.");
+        
+        Console.Write("Proszę podać pierwszą liczbę całkowitą: ");
+        int pierwsza =  int.Parse(Console.ReadLine());
+        
+        Console.Write("Proszę podać drugą liczbę całkowitą: ");
+        int druga = int.Parse(Console.ReadLine());
+
+        if (pierwsza % druga == 0)
+        {
+            Console.WriteLine($"{druga} jest dzielnikiem {pierwsza}");
+        }
+        else
+        {
+            Console.WriteLine($"{druga} nie jest dzielnikiem  {pierwsza}");
+        }
+    }
+}
