5495a7053ebaa5f3a65772030445e893044d3ac4
Adrian Łaciak <adrianlaciak02@gmail.com>
2026-01-18 17:41:40 +0100

    Factorial

---
+++ CSharp/Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Factorial;
+
+/*
+Napisz program obliczający n! (n silnia), gdzie n jest podane przez użytkownika.
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Proszę podać liczbę: ");
+        int n =  int.Parse(Console.ReadLine());
+        int wynik = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            wynik *= i;
+        }
+        Console.WriteLine($"{n}! = {wynik}");
+    }
+}
