dca76a68bff547702f1661d63ba2fc5bed1a87f5
Adrian Łaciak <adrianlaciak02@gmail.com>
2026-01-18 17:46:39 +0100

    How many numbers to reach 100

---
+++ CSharp/HowManyNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HowManyNumbers;
+
+/*Napisz program obliczający ile kolejnych liczb całkowitych (rozpoczynając od wartości
+1) należy dodać do siebie, aby suma przekroczyła wartość 100.*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int wartosc = 100;
+        int suma = 0;
+        int licznik = 0;
+
+        for (int i = 1; suma <= wartosc; i++)
+        {
+            suma += i;
+            licznik++;
+        }
+        Console.WriteLine($"Potrzeba {licznik} liczb.");
+    }
+}
