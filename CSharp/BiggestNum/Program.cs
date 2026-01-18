f5c8a766ba6110f2272f3ac89e3b849c3ee62a0b
Adrian Łaciak <adrianlaciak02@gmail.com>
2026-01-18 17:14:57 +0100

    Biggest number

---
+++ CSharp/BiggestNum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BiggestNum;
+
+/*Napisz program pobierający od użytkownika 3 liczby. Program ma wyświetlić wartość
+największej z nich.*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Proszę podać 1 liczbę: ");
+        double pierwsza = double.Parse(Console.ReadLine());
+        Console.WriteLine("Proszę podać 2 liczbę: ");
+        double druga =  double.Parse(Console.ReadLine());
+        Console.WriteLine("Proszę podać 3 liczbę: ");
+        double trzecia = double.Parse(Console.ReadLine());
+
+        double najwieksza = pierwsza;
+        if (druga > najwieksza)
+        {
+            najwieksza = druga;
+        }
+
+        if (trzecia > najwieksza)
+        {
+            najwieksza = trzecia;
+        }
+        
+        Console.WriteLine($"Spośród liczb {pierwsza}; {druga}; {trzecia} największa to {najwieksza}.");
+
+    }
+}
