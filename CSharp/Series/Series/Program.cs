f1faad2bbe858b51ed072b2dc737e029ffe5d542
adrianlaciak <adrianlaciak02@gmail.com>
2026-01-19 20:45:26 +0100

    Series

---
+++ CSharp/Series/Series/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Series
+
+/*    Napisz program obliczający sumę szeregu W(n)=1 – 2 + 3 – 4 + ...± n, gdzie n jest
+dowolną liczbą naturalną, którą program ma wczytać.*/
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proszę podać liczbę: ");
+            int n = int.Parse(Console.ReadLine());
+            int suma_plus = 0;
+            int suma_minus = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    suma_minus += i;
+                }
+                else
+                {
+                    suma_plus += i;
+                }
+            }
+            int wynik = suma_plus - suma_minus;
+            Console.WriteLine($"Suma szeregu wynosi {wynik}.");
+        }
+    }
+}
